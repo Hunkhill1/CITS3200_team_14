661b0e7ecfeb2b371a2b1a9926bef6518f7056aa
Ethan-Wilson2 <23096565@student.uwa.edu.au>
2023-08-23 12:49:35 +0800

    Additional base set up

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,20 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
 @app.route('/')
-@app.route('/login')
-def login():
-    form = LoginForm()
-    return render_template('login.html', title='Sign In', form=form)
-
 @app.route('/index')
 def index():
     user = {'username': 'Miguel'}
     return render_template('index.html', title='Home', user=user)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
+
 
