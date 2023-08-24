8bfd6f02be4b44a0a6dd117d887fe5401e5f9217
Sahil Narula <126839546+Hunkhill1@users.noreply.github.com>
2023-08-24 09:28:50 +0800

    WIP on planner.html, added routing and tried to work with the drop down boxes, working so far

--- app/routes.py
+++ app/routes.py
@@ -10,18 +10,8 @@ from werkzeug.urls import url_parse
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template("index.html", title='Home Page')
+    
+    return render_template("planner.html", title='Planner')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -50,7 +40,7 @@ def logout():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('planner'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -60,3 +50,9 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/planner')
+@login_required
+def planner():
+
+    return render_template("planner.html", title='Planner')
