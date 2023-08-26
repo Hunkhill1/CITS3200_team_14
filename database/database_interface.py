5770111fbdd355be13a94ee13c248a11f5fb9f00
Iash Bashir <iash.bashir5225@gmail.com>
2023-08-26 23:06:36 +0800

    added in "add unit to db" function that uses terminal

--- database/database_interface.py
+++ database/database_interface.py
@@ -20,7 +20,38 @@ def get_prerequisites(unit_code):
     
     return prerequisites
 
+def insert_unit_data():
+    unit_data = []
+
+    while True:
+        unit_code = input("Enter unit code (or 'exit' to finish): ")
+        if unit_code.lower() == 'exit':
+            break
+
+        unit_name = input("Enter unit name: ")
+        semester = int(input("Enter semester: "))
+
+        unit_data.append((unit_code, unit_name, semester, 'incomplete'))
+
+    # Connect to the database
+    connection = sqlite3.connect('database/degree_database.db')
+    cursor = connection.cursor()
+
+    # Insert unit data into the Unit table
+    insert_unit_query = """
+    INSERT INTO Unit (code,name,semester,status) VALUES (?, ?, ?, ?)
+    """
+    cursor.executemany(insert_unit_query, unit_data)
+
+    # Commit the changes and close the connection
+    connection.commit()
+    connection.close()
+
+    print("Unit data inserted successfully.")
+
 # Example usage
-unit_code = "GENG2003"
-prerequisites = get_prerequisites(unit_code)
-print(f"Prerequisites for unit {unit_code}: {prerequisites}")
+# unit_code = "GENG2003"
+# prerequisites = get_prerequisites(unit_code)
+# print(f"Prerequisites for unit {unit_code}: {prerequisites}")
+if __name__ == "__main__":
+    insert_unit_data()
