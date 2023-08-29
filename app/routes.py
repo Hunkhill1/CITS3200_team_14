a970311c4e7320a918c6a053b505d73ad5ae7e51
Sahil Narula <126839546+Hunkhill1@users.noreply.github.com>
2023-08-29 13:02:19 +0800

    Got prerequisites working and displaying on screen when selected from drop down box

--- app/routes.py
+++ app/routes.py
@@ -21,7 +21,12 @@ def index_route():
 
 unit = Blueprint('unit', __name__)
 
-@unit.route('/unit/<unit_code>')
+from flask import Blueprint, render_template, request, jsonify
+import sqlite3
+
+# ... (other code)
+
+@unit.route('/<unit_code>')
 def unit_route(unit_code):
     # Connect to the database
     conn = sqlite3.connect('database/degree_database.db')
