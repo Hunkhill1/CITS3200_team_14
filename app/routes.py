b3b35fd0e65fa31cc5a0d846b1d5eca06c78b41b
Ethan-Wilson2 <23096565@student.uwa.edu.au>
2023-09-06 14:47:09 +0800

    Default plan is now created

--- app/routes.py
+++ app/routes.py
@@ -42,16 +42,31 @@ def planner_route():
     # Connect to the database
     conn = sqlite3.connect('database/degree_database.db')
     cursor = conn.cursor()
-
-    # Fetch data from the Unit table
     cursor.execute("SELECT code, name, semester, status FROM Unit")
     units_from_database = cursor.fetchall()
-
-    # Close the database connection
     conn.close()
 
-    # Pass the fetched data to the template for rendering
-    return render_template('planner.html', units=units_from_database)
+    # Define the default plan
+    default_plan = {
+        'year_1': {
+            'semester_1': ['MATH1011', 'GENG1010', 'PHYS1001', 'CITS2401'],
+            'semester_2': ['MATH1012', 'ENSC1004', 'ENSC2004', 'GENG1101'],
+        },
+        'year_2': {
+            'semester_1': ['GENG2003', 'GENG2004', 'MECH2002', 'ENSC2003'],
+            'semester_2': ['MATH3023', 'MECH2004', 'MECH3024', 'Select unit'],
+        },
+        'year_3': {
+            'semester_1': ['MECH3002', 'MECH4429', 'Select unit', 'Select unit'],
+            'semester_2': ['GENG3405', 'MECH3001', 'MECH3424', 'MECH4502'],
+        },
+        'year_4': {
+            'semester_1': ['MECH5551', 'MECH4426', 'Select unit', 'Select unit'],
+            'semester_2': ['GENG5507', 'GENG3402', 'Select unit', 'Select unit'],
+        }
+    }
+
+    return render_template('planner.html', default_plan=default_plan, all_units=units_from_database)
 
 
 
