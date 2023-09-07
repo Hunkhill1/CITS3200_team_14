618be446947b78f448f26d7049f5a4970bcd9123
Iash Bashir <iash.bashir5225@gmail.com>
2023-09-07 22:43:26 +0800

    working on adding units by available semester - still has bugs

--- database/matrix.py
+++ database/matrix.py
@@ -1,6 +1,7 @@
-import tkinter as tk
-from tkinter import ttk
 import sqlite3
+from typing import List
+import datetime
+
 
 # Create an SQLite database and a table to store study units
 conn = sqlite3.connect('study_units.db')
@@ -19,62 +20,25 @@ cursor.execute('''
 ''')
 conn.commit()
 
-# Function to add a study unit to the database
-def add_study_unit(semester, unit_1, unit_2, unit_3, unit_4):
-    cursor.execute('''
-        INSERT INTO study_units (semester, unit_1, unit_2, unit_3, unit_4)
-        VALUES (?, ?, ?, ?, ?)
-    ''', (semester, unit_1, unit_2, unit_3, unit_4))
-    conn.commit()
-
 # Function to retrieve study units from the database
-def get_study_units():
+def get_study_units()->List[str]:
     cursor.execute('SELECT * FROM study_units')
     return cursor.fetchall()
 
-# Function to display the study matrix in a GUI window
-def display_study_matrix_gui():
-    study_units = get_study_units()
-
-    if not study_units:
-        print("No study units added yet.")
-        return
-
-    # Create a tkinter window
-    window = tk.Tk()
-    window.title("Study Matrix")
-
-    # Create a treeview widget (table) to display the data
-    tree = ttk.Treeview(
-        window,
-        columns=["Semester", "Unit 1", "Unit 2", "Unit 3", "Unit 4"],
-        show="headings",
-    )
-    tree.heading("Semester", text="Semester")
-    tree.heading("Unit 1", text="Unit 1")
-    tree.heading("Unit 2", text="Unit 2")
-    tree.heading("Unit 3", text="Unit 3")
-    tree.heading("Unit 4", text="Unit 4")
-
-    # Insert the data into the treeview
-    for row in study_units:
-        tree.insert("", "end", values=row[1:])
-
-    # Pack the treeview
-    tree.pack()
-
-    # Start the tkinter main loop
-    window.mainloop()
-
 # Function to update the semester column in the database
 def update_semester_column():
-    current_year = 2023  # Change this to the desired starting year
+    today = datetime.date.today()
+    current_year = today.year  # Change this to the desired starting year
+    
+    # Add the semester column to the database if it doesn't exist      
     for semester in range(1, 7):
+        # Alternate between 1 and 2 for the semester number
+        semester_number = 1 if semester % 2 == 1 else 2
+        
         cursor.execute('''
-            UPDATE study_units
-            SET semester = ?
-            WHERE id = ?
-        ''', (f"Semester {semester}, {current_year}", semester))
+            INSERT INTO study_units (id, semester)
+            VALUES (?, ?)
+        ''', (semester, f"Semester {semester_number}, {current_year}"))
         if semester % 2 == 0:
             current_year += 1
     conn.commit()
@@ -98,26 +62,13 @@ def add_unit_to_matrix(unit_code: str, semester: int):
 
 def run():
     # Example usage:
-    update_semester_column()
+    #update_semester_column()
 
     # Example usage:
     add_unit_to_matrix("PHYS1001", 1)
     add_unit_to_matrix("MATH1011", 1)
     add_unit_to_matrix("CITS2401", 1)
     add_unit_to_matrix("ENSC1001", 1)
-
-    add_unit_to_matrix("Test1", 1)
-    add_unit_to_matrix("Test2", 1)
-    add_unit_to_matrix("Test3", 1)
-    add_unit_to_matrix("Test4", 1)
-
-    add_unit_to_matrix("Test5", 1)
-    add_unit_to_matrix("Test6", 1)
-    add_unit_to_matrix("Test7", 1)
-    add_unit_to_matrix("Test8", 1)
-
-    # Display the study matrix in a GUI window
-    display_study_matrix_gui()
 
 run()
 
