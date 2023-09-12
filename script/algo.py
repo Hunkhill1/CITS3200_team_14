768a02a37023de4a66cf9fe8b545c3bde1a41f72
Iash Bashir <iash.bashir5225@gmail.com>
2023-09-12 21:52:10 +0800

    cleaned up algo

--- script/algo.py
+++ script/algo.py
@@ -1,12 +1,19 @@
 from script.study_planner_interface import update_semester_column, add_unit_to_matrix
 from script.available_units import CanDo
 
-update_semester_column()
-
 completed_units = ['a','b','c']
 incomplete_units = [['d',['a']],['e',['a','b']],['f',['l','m','n']]]
 
-post_req = CanDo(completed_units, incomplete_units)
-for units in  post_req:
-    add_unit_to_matrix(units, 1)
+def algorithm(completed_units, incomplete_units)->None:
+    """ Algorithm to add units to the study plan matrix
 
+    Args:
+        completed_units (str): list of completed unit codes
+        incomplete_units (str): list of uncompleted unit codes
+    """
+    update_semester_column()
+    post_req = CanDo(completed_units, incomplete_units)
+    for units in  post_req:
+        add_unit_to_matrix(units, 1)
+        
+algorithm(completed_units, incomplete_units)
