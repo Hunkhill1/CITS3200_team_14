e4e73d7aea0fff34f06d1702bb7eb756fb91a14e
Iash Bashir <iash.bashir5225@gmail.com>
2023-09-17 12:26:19 +0800

    added constants to route.py

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, jsonify, flash, redirect
 import sqlite3
 from app.forms import LoginForm
+import script.constants as constants
 
 index = Blueprint('index', __name__)
 unit = Blueprint('unit', __name__)
@@ -8,7 +9,7 @@ unit = Blueprint('unit', __name__)
 @index.route('/')
 def index_route():
     # Connect to the database
-    conn = sqlite3.connect('/home/long/Desktop/Professional Computing/CITS3200_team_14/database/degree_database.db')
+    conn = sqlite3.connect(constants.degree_db_address)
     cursor = conn.cursor()
 
     # Fetch data from the Unit table
@@ -24,7 +25,7 @@ def index_route():
 @unit.route('/<unit_code>')
 def unit_route(unit_code):
     # Connect to the database
-    conn = sqlite3.connect('/home/long/Desktop/Professional Computing/CITS3200_team_14/database/degree_database.db')
+    conn = sqlite3.connect(constants.degree_db_address)
     cursor = conn.cursor()
 
     # Fetch prerequisites for the specified unit
@@ -40,7 +41,7 @@ def unit_route(unit_code):
 @index.route('/planner')
 def planner_route():
     # Connect to the database
-    conn = sqlite3.connect('/home/long/Desktop/Professional Computing/CITS3200_team_14/database/degree_database.db')
+    conn = sqlite3.connect(constants.degree_db_address)
     cursor = conn.cursor()
     cursor.execute("SELECT code, name, semester, status FROM Unit")
     units_from_database = cursor.fetchall()
