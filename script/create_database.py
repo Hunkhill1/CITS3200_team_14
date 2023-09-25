bdfe9d49ffd24ffedc3d8ce50d22822dfaea9041
Iash Bashir <iash.bashir5225@gmail.com>
2023-09-25 13:59:04 +0800

    added optional units to database

--- script/create_database.py
+++ script/create_database.py
@@ -5,18 +5,44 @@ import constants as constants
 connection = sqlite3.connect(constants.degree_db_address)
 cursor = connection.cursor()
 
-# Create the Unit table with unit points
+# Create the UnitCategory table to store unit categories
+create_unit_category_table_query = """
+CREATE TABLE IF NOT EXISTS UnitCategory (
+    category_id INTEGER PRIMARY KEY,
+    category_name TEXT NOT NULL
+);
+"""
+cursor.execute(create_unit_category_table_query)
+
+# Insert data into the UnitCategory table for the categories
+categories_data = [
+    ('core',),
+    ('broadening',),
+    ('option',),
+    ('Group A',),  # Sub-category 1 for option units
+    ('Group B',),  # Sub-category 2 for option units
+]
+
+insert_category_query = """
+INSERT INTO UnitCategory (category_name) VALUES (?)
+"""
+
+cursor.executemany(insert_category_query, categories_data)
+
+# Create the Unit table with unit points and the new category_id column
 create_unit_table_query = """
 CREATE TABLE IF NOT EXISTS Unit (
-    code TEXT PRIMARY KEY, 
+    code TEXT PRIMARY KEY,
     name TEXT,
     unit_points_required INTEGER,
-    semester INTEGER
+    semester INTEGER,
+    category_id INTEGER,
+    FOREIGN KEY (category_id) REFERENCES UnitCategory (category_id)
 );
 """
 cursor.execute(create_unit_table_query)
 
-# Modify the UnitRelationship table to reference unit points
+# Modify the UnitRelationship table to reference unit points and categories
 create_unit_relationship_table_query = """
 CREATE TABLE IF NOT EXISTS UnitRelationship (
     unit_code TEXT,
