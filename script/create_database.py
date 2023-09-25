a9426cf433806a2adc40c9812c9de34752eda4f3
Iash Bashir <iash.bashir5225@gmail.com>
2023-09-25 12:54:30 +0800

    in_progress - updating db scheme to include unit points

--- script/create_database.py
+++ script/create_database.py
@@ -5,24 +5,27 @@ import constants as constants
 connection = sqlite3.connect(constants.degree_db_address)
 cursor = connection.cursor()
 
-# Create the Unit table
+# Create the Unit table with unit points
 create_unit_table_query = """
 CREATE TABLE IF NOT EXISTS Unit (
     code TEXT PRIMARY KEY, 
-    name TEXT ,
+    name TEXT,
+    unit_points INTEGER,  -- New column for unit points
     semester INTEGER,
     status TEXT DEFAULT 'incomplete'
 );
 """
 cursor.execute(create_unit_table_query)
 
-# Create the UnitRelationship table
+# Modify the UnitRelationship table to reference unit points
 create_unit_relationship_table_query = """
 CREATE TABLE IF NOT EXISTS UnitRelationship (
     unit_code TEXT,
     pre_requisite TEXT,
     FOREIGN KEY (unit_code) REFERENCES Unit (code),
-    FOREIGN KEY (pre_requisite) REFERENCES Unit (code)
+    FOREIGN KEY (pre_requisite) REFERENCES Unit (code),
+    unit_points_required INTEGER,  -- New column for unit points required as a prerequisite
+    PRIMARY KEY (unit_code, pre_requisite)
 );
 """
 cursor.execute(create_unit_relationship_table_query)
@@ -31,4 +34,4 @@ cursor.execute(create_unit_relationship_table_query)
 connection.commit()
 connection.close()
 
-print("Database schema created successfully.")
+print("Database schema updated successfully.")
