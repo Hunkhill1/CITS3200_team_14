24a1b300f37af7ca638699ec7da081169e9e0007
Sahil Narula <126839546+Hunkhill1@users.noreply.github.com>
2023-10-09 00:07:35 +0800

    edited Proejct.py

--- Project.py
+++ Project.py
@@ -10,95 +10,93 @@ import script.constants as constants
 import json
 import os
 
-def create_app():
+#def create_app():
 
-    app = Flask(__name__, template_folder=constants.template_folder_address, static_folder=constants.static_folder_address)
-    app.config.from_object(Config)
-    app.secret_key = 'your_secret_key'
-    login = LoginManager(app) # type: ignore
-    login.login_view = 'login'
+app = Flask(__name__, template_folder=constants.template_folder_address, static_folder=constants.static_folder_address)
+app.config.from_object(Config)
+app.secret_key = 'your_secret_key'
+login = LoginManager(app) # type: ignore
+login.login_view = 'login'
 
 
 
-    @login.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
-    app.register_blueprint(index)
-    app.register_blueprint(unit, url_prefix='/unit')
-    #app.register_blueprint(staff_editing_bp)
+app.register_blueprint(index)
+app.register_blueprint(unit, url_prefix='/unit')
+#app.register_blueprint(staff_editing_bp)
 
-    @app.route('/login', methods=['GET', 'POST'])
-    def login():
-        form = LoginForm()
-        if form.validate_on_submit():
-            flash('Login requested for user {}, remember_me={}'.format(
-                form.username.data, form.remember_me.data))
-            return redirect(url_for('staff_editing'))
-        return render_template('login.html', title='Sign In', form=form)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('staff_editing'))
+    return render_template('login.html', title='Sign In', form=form)
 
-    @app.route('/logout', methods=['POST'])
-    def logout():
-        logout_user()  # Log the user out
-        flash('Logged out successfully!')
-        dummy_form = LoginForm()
-        return render_template('login.html', title='Sign In', form=dummy_form)  # Redirect to the login page after logout
+@app.route('/logout', methods=['POST'])
+def logout():
+    logout_user()  # Log the user out
+    flash('Logged out successfully!')
+    dummy_form = LoginForm()
+    return render_template('login.html', title='Sign In', form=dummy_form)  # Redirect to the login page after logout
 
-    @app.route('/planner', methods=['GET'])
-    def planner():
-        return render_template('planner.html', title='Unit Planner')
+@app.route('/planner', methods=['GET'])
+def planner():
+    return render_template('planner.html', title='Unit Planner')
 
-    @app.route('/staff_editing')
-    def staff_editing():
-        return render_template('staff_editing.html', title='Staff Editing Page')
+@app.route('/staff_editing')
+def staff_editing():
+    return render_template('staff_editing.html', title='Staff Editing Page')
 
-    @app.route('/process_json', methods=['POST'])
-    def process_json_data():
-        try:
-            json_data = request.get_json()
-            
-            # Process the JSON data as needed
-            # print("Received JSON data:")
-            # print(json.dumps(json_data, indent=4))  # Pretty-print JSON data
-
-            # Initialize lists to store complete and incomplete units
-            complete_units = []
-            incomplete_units = []
-
-            # Loop through the JSON data and categorize units based on status
-            for unit_code, status in json_data.items():
-                if status == 'complete':
-                    complete_units.append(unit_code)
-                elif status == 'incomplete':
-                    incomplete_units.append(unit_code)
+@app.route('/process_json', methods=['POST'])
+def process_json_data():
+    try:
+        json_data = request.get_json()
         
-            filtered_incomplete_units = clean_list(incomplete_units)
-            filtered_complete_units = clean_list(complete_units) 
-            print(f"Trimmed incomplete units: {filtered_incomplete_units}")
-            print(f"Trimmed complete units: {filtered_complete_units}")   
+        # Process the JSON data as needed
+        # print("Received JSON data:")
+        # print(json.dumps(json_data, indent=4))  # Pretty-print JSON data
 
-            # Pretty-print the lists of complete and incomplete units
-            print("Complete Units:")
-            print(json.dumps(complete_units, indent=4))
-            print("Incomplete Units:")
-            print(json.dumps(incomplete_units, indent=4))
+        # Initialize lists to store complete and incomplete units
+        complete_units = []
+        incomplete_units = []
+
+        # Loop through the JSON data and categorize units based on status
+        for unit_code, status in json_data.items():
+            if status == 'complete':
+                complete_units.append(unit_code)
+            elif status == 'incomplete':
+                incomplete_units.append(unit_code)
+    
+        filtered_incomplete_units = clean_list(incomplete_units)
+        filtered_complete_units = clean_list(complete_units) 
+        print(f"Trimmed incomplete units: {filtered_incomplete_units}")
+        print(f"Trimmed complete units: {filtered_complete_units}")   
+
+        # Pretty-print the lists of complete and incomplete units
+        print("Complete Units:")
+        print(json.dumps(complete_units, indent=4))
+        print("Incomplete Units:")
+        print(json.dumps(incomplete_units, indent=4))
 
 
-            start_year_semester = int(json_data["startYearSemester"])
-            print(start_year_semester)   
-                
-            algorithm(filtered_incomplete_units, filtered_complete_units, start_year_semester)
+        start_year_semester = int(json_data["startYearSemester"])
+        print(start_year_semester)   
+            
+        algorithm(filtered_incomplete_units, filtered_complete_units, start_year_semester)
 
-            # Return a response if needed
-            response_data = {'message': 'Data received successfully'}
-            return jsonify(response_data), 200
-        except Exception as e:
-            return jsonify({'error': str(e)}), 400
+        # Return a response if needed
+        response_data = {'message': 'Data received successfully'}
+        return jsonify(response_data), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 
-# if __name__ == '__main__':
-#     #app.run(debug=True)
-#     app = create_app()
-#     #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+if __name__ == '__main__':
+    app.run()
 
 
