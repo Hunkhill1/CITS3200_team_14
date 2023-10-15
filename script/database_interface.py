5b830c3cf3636dc4243cdbc909cbf231566d8513
Iash Bashir <iash.bashir5225@gmail.com>
2023-10-15 21:26:00 +0800

    added backend functionality,and can be called from Project.oy

--- script/database_interface.py
+++ script/database_interface.py
@@ -44,8 +44,76 @@ def insert_unit_data(unit_data: list[tuple]) -> None:
         INSERT INTO Unit (code, name, semester) VALUES (?, ?, ?)
         """
         cursor.executemany(insert_unit_query, unit_data)
+        
+def add_unit(unit_code: str, name: str, unit_points_required: int, semester: int, category_id: int) -> None:
+    
+    with sqlite3.connect(constants.degree_db_address) as connection:
+        cursor = connection.cursor()
 
+        insert_unit_query = """
+        INSERT INTO Unit (code, name, unit_points_required, semester, category_id) VALUES (?, ?, ?, ?, ?)
+        """
+        cursor.execute(insert_unit_query, (unit_code, name, unit_points_required, semester, category_id))
+    connection.close()
+        
+def delete_unit_by_code(unit_code: str) -> None:
+    """Delete a unit by its code from the database.
 
+    Args:
+        unit_code (str): Unit code to delete.
+    """
+    with sqlite3.connect(constants.degree_db_address) as connection:
+        cursor = connection.cursor()
+
+        # Delete the unit based on its code
+        delete_query = """
+        DELETE FROM Unit
+        WHERE code = ?
+        """
+        cursor.execute(delete_query, (unit_code,))
+
+    # Close the connection
+    connection.close()
+
+def edit_units(unit_code: str, name: str, unit_points_required: int, semester: int, category_id: int) -> None:
+    """Edit unit attributes in the database.
+
+    Args:
+        unit_code (str): Unit code to identify the unit to be edited.
+        name (str): New unit name.
+        unit_points_required (int): New unit points required.
+        semester (int): New unit semester.
+        category_id (int): New category ID.
+
+    Raises:
+        Exception: If the unit does not exist in the database.
+    """
+    # Create or connect to the database
+    connection = sqlite3.connect(constants.degree_db_address)
+    cursor = connection.cursor()
+
+    # Check if the unit exists in the database
+    cursor.execute("SELECT COUNT(*) FROM Unit WHERE code=?", (unit_code,))
+    unit_exists = cursor.fetchone()[0]
+
+    if unit_exists == 0:
+        connection.close()
+        raise Exception("Unit not found in the database.")
+
+    # Update the unit attributes
+    update_unit_query = """
+    UPDATE Unit
+    SET name=?, unit_points_required=?, semester=?, category_id=?
+    WHERE code=?
+    """
+    cursor.execute(update_unit_query, (name, unit_points_required, semester, category_id, unit_code))
+
+    # Commit the changes and close the connection
+    connection.commit()
+    connection.close()
+
+    print(f"Unit '{unit_code}' updated successfully.")
+    
 def get_unit_semester(unit_code: str) -> int:
     """Get the semester of a unit
 
